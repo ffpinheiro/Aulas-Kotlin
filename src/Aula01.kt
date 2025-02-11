eccbba13188edc53a9cfdfe9f2a831a5cac4ef14
Fabricio <68662782+ffpinheiro@users.noreply.github.com>
2025-02-11 13:47:00 -0300

    Aula01

---
+++ src/Aula01.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Hello World, Kotlin")
+    var x = 10
+    val nome = "João"
+    println(x)
+    println(nome)
+//    nome = "Maria" // Gera erro por tentar mudar o valor de variável imutável
+}
